d181e357ebc2bd32582eabfa3b2d06415458d54d
Dealsa44 <z_tavartkiladze@cu.edu.ge>
2025-05-01 15:34:06 +0400

    e email link functionality added to footerfor mobile devices

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -31,9 +31,6 @@ export class FooterComponent {
     window.open('https://www.google.com/maps/place/UGT+Cloudforce/@41.7107022,44.76066,15z/data=!4m10!1m2!2m1!1sUGT!3m6!1s0x40447340ad02f8e7:0xcc3666222f873076!8m2!3d41.7107154!4d44.7709595!15sCgNVR1QiA4gBAZIBG2J1c2luZXNzX25ldHdvcmtpbmdfY29tcGFueaoBNBABKgciA3VndCgAMh4QASIaRKqMXp5C_g9cngE6X6yY7jdtupMXH4BPjd8yBxACIgN1Z3TgAQA!16s%2Fg%2F11flzg5fhc?entry=ttu&g_ep=EgoyMDI1MDQyMy4wIKXMDSoJLDEwMjExNDUzSAFQAw%3D%3D');
   }
 
-  openEmail(): void {
-    window.open('https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSHxwLZqNrbcwTrFHTwvTtRNgfcNmLgSZLsTqXchhRHWFwmlXfwfjvtFsLFvxrbwMgGXMQCS');
-  }
 
   openSocial(url: string): void {
     window.open(url, '_blank');
@@ -43,5 +40,19 @@ export class FooterComponent {
       window.location.href = 'tel:+995322220505';
     }
     // On desktop, do nothing (just the text will be displayed)
+  }
+  handleEmailClick(): void {
+    if (this.isMobileDevice()) {
+      // For mobile devices - open default email app
+      window.location.href = 'mailto:ugt@ugt.ge';
+    } else {
+      // For desktop - keep existing behavior
+      this.openEmail();
+    }
+  }
+  
+  // Keep the existing openEmail for desktop
+  openEmail(): void {
+    window.open('https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSHxwLZqNrbcwTrFHTwvTtRNgfcNmLgSZLsTqXchhRHWFwmlXfwfjvtFsLFvxrbwMgGXMQCS');
   }
 }
