ec528e94d97649c5ab2c3262e08ef8e9b18cb8ad
Dealsa44 <z_tavartkiladze@cu.edu.ge>
2025-06-13 13:01:56 +0400

    .

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -37,7 +37,7 @@ export class FooterComponent {
   }
 
   openMap(): void {
-    window.open('https://www.google.com/maps/place/UGT+Cloudforce/@41.7107022,44.76066,15z/data=!4m10!1m2!2m1!1sUGT!3m6!1s0x40447340ad02f8e7:0xcc3666222f873076!8m2!3d41.7107154!4d44.7709595!15sCgNVR1QiA4gBAZIBG2J1c2luZXNzX25ldHdvcmtpbmdfY29tcGFueaoBNBABKgciA3VndCgAMh4QASIaRKqMXp5C_g9cngE6X6yY7jdtupMXH4BPjd8yBxACIgN1Z3TgAQA!16s%2Fg%2F11flzg5fhc?entry=ttu&g_ep=EgoyMDI1MDQyMy4wIKXMDSoJLDEwMjExNDUzSAFQAw%3D%3D');
+    window.open('https://maps.app.goo.gl/4zFFzBTHFMBdebpN7');
   }
 
   openSocial(url: string): void {
